adb0cbdadcf63898ee8b0b536d928cca0bcae0b8
AyKrimino <ayoub.krimi14@gmail.com>
2024-01-21 22:32:43 +0100

    Adding the display tasks page

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    
+    path('', views.display_tasks, name='display-tasks'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def display_tasks(request):
+    return render(request, 'tasks/tasks.html')
