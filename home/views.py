02587240624e3a4596a61597c0a6464bb7da405c
AyKrimino <ayoub.krimi14@gmail.com>
2024-01-24 22:11:59 +0100

    Start working on login page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .forms import UserForm
 
 
 def home(request):
     return render(request, 'home/index.html')
+
+
+def login(request):
+    form = UserForm()
+    return render(request, 'home/login.html', {'form': form})
